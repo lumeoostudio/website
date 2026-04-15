dbdd963a6050678633ebe6e1ac822b5de45eaa1e
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2026-04-15 10:33:57 +0100

    Update configuration and styles: adjusted line width in biome.json, enabled organize imports, added new dependencies in package.json and bun.lock, modified Vite configuration to include Nitro plugin, and enhanced Header component with navigation links and email button.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,54 @@
+import { Link } from "@tanstack/react-router";
+
 export const Header = () => {
-	return <header className="">d</header>;
+	return (
+		<nav className="mx-auto flex max-h-25 min-h-25 w-full max-w-225 items-center justify-between px-6 py-7">
+			<img
+				src="/assets/brand/logo-large.svg"
+				alt="Logo"
+				className="h-8 max-w-33.25 flex-1"
+			/>
+			<ul className="flex items-center gap-8">
+				<li>
+					<Link
+						to="/"
+						className="font-medium text-base text-secondary leading-[1.4] duration-200 ease-in-out hover:text-primary"
+					>
+						Process
+					</Link>
+				</li>
+				<li>
+					<Link
+						to="/"
+						className="font-medium text-base text-secondary leading-[1.4] duration-200 ease-in-out hover:text-primary"
+					>
+						Work
+					</Link>
+				</li>
+				<li>
+					<Link
+						to="/"
+						className="font-medium text-base text-secondary leading-[1.4] duration-200 ease-in-out hover:text-primary"
+					>
+						Pricing
+					</Link>
+				</li>
+				<li>
+					<Link
+						to="/"
+						className="font-medium text-base text-secondary leading-[1.4] duration-200 ease-in-out hover:text-primary"
+					>
+						Testimonial
+					</Link>
+				</li>
+			</ul>
+
+			<a
+				href="mailto:hello@lumeoo.studio"
+				className="rounded-full border border-[#353535] bg-primary px-5 py-2.5 text-base text-white leading-6 tracking-tight shadow-[-13px_20px_18px_rgba(0,0,0,0.02),-7px_15px_15px_rgba(0,0,0,0.05),-3px_11px_11px_rgba(0,0,0,0.08),-1px_3px_6px_rgba(0,0,0,0.1)] backdrop-blur-[12.5px]"
+			>
+				Email Us
+			</a>
+		</nav>
+	);
 };

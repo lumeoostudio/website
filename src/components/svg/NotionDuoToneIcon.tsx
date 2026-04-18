07ccecf9a4a43d78bb91618af7987346349ef568
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2026-04-18 10:37:23 +0100

    Refactor FigmaDuoToneIcon and NotionDuoToneIcon components to use camelCase for stroke properties, enhancing consistency and adherence to JSX standards.

--- src/components/svg/NotionDuoToneIcon.tsx
+++ src/components/svg/NotionDuoToneIcon.tsx
@@ -12,16 +12,16 @@ export const NotionDuoToneIcon = (props: React.SVGProps<SVGSVGElement>) => {
 			<path
 				d="M2.5 3.5L16.5 2.5L21.5 5.5M2.5 3.5L6.5 6.5M2.5 3.5V16.5L6.5 21.5M21.5 5.5L6.5 6.5M21.5 5.5V19.5L6.5 21.5M6.5 6.5V21.5"
 				stroke="currentColor"
-				stroke-width="1.5"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<path
 				d="M12.4704 17.7039L9.5 18.0999M18.5 8.69995L16.2654 8.84893M17.5 8.76662V16.9999L11.7148 9.1523L9.5 9.29995M11 9.19995V17.6999"
 				stroke="white"
-				stroke-width="1.5"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</svg>
 	);
